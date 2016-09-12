70db2b7ac406ab527f07de152d35b6a9a4f43cc3
Jason Hanna <jason.hanna01@gmail.com>
2016-09-12 10:05:55 -0600

    start refactoring of method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,20 @@
 class SearchController < ApplicationController
 
   def index
-    @connection        = Faraday.new('https://developer.nrel.gov')
+    @stations = search_locations_by_zipcode
+  end
+
+  def search_locations_by_zipcode
+    @connection        = Faraday.new("https://developer.nrel.gov")
     @connection.headers["X-Api-Key"] = ENV["NREL_API_KEY"]
     response = @connection.get("/api/alt-fuel-stations/v1/nearest.json") do |req|
       req.params['location'] = params[:zip]
     end
-
-    @stations = response.body
+    parse(response)
   end
 
+  def parse(response)
+    JSON.parse(response.body)
+  end
 
 end
