03416163b9377cf3c45b3c827f16c43298094e8d
Jason Hanna <jason.hanna01@gmail.com>
2016-09-12 09:51:08 -0600

    add parameters to search#index method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,11 @@ class SearchController < ApplicationController
   def index
     @connection        = Faraday.new('https://developer.nrel.gov')
     @connection.headers["X-Api-Key"] = ENV["NREL_API_KEY"]
+    response = @connection.get("/api/alt-fuel-stations/v1/nearest.json") do |req|
+      req.params['location'] = params[:zip]
+    end
 
-    @stations = 
+    @stations = response.body
   end
 
 
