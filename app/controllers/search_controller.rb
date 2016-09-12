135a4b94e9f7648d134935db820fecb50f2416e4
Jason Hanna <jason.hanna01@gmail.com>
2016-09-12 09:36:21 -0600

    add nrel api key - start implenting index method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
 
   def index
+    @connection        = Faraday.new('https://developer.nrel.gov')
+    @connection.headers["X-Api-Key"] = ENV["NREL_API_KEY"]
 
+    @stations = 
   end
 
 
